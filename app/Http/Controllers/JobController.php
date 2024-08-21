6faf62540cbd9c0d84d4cba67860d4641cc9d765
ciprian <ciprianvlad888@gmail.com>
2024-08-21 21:00:29 +0100

    refactored routes and added Job Policy

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -3,76 +3,75 @@
 namespace App\Http\Controllers;
 
 use App\Models\Job;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 
-
 class JobController extends Controller
 {
-    public function index() 
+    public function index()
     {
         $jobs = Job::with('employer')->latest()->simplePaginate(3);
 
-    return view('jobs.index', [
-        'jobs' => $jobs
-    ]);
+        return view('jobs.index', [
+            'jobs' => $jobs
+        ]);
     }
-    
-    public function create() 
+
+    public function create()
     {
         return view('jobs.create');
     }
-    public function show(Job $job) 
+
+    public function show(Job $job)
     {
         return view('jobs.show', ['job' => $job]);
     }
-    public function store() 
+
+    public function store()
     {
         request()->validate([
             'title' => ['required', 'min:3'],
             'salary' => ['required']
         ]);
-    
+
         Job::create([
             'title' => request('title'),
             'salary' => request('salary'),
             'employer_id' => 1
         ]);
-    
+
         return redirect('/jobs');
     }
-    public function edit(Job $job) 
+
+    public function edit(Job $job)
     {
-
-        Gate::authorize('edit-job', $job);
-
         return view('jobs.edit', ['job' => $job]);
     }
-    public function update(Job $job) 
+
+    public function update(Job $job)
     {
-    // authorize (On hold...)
-    $job->update([
-        'title' => request('title'),
-        'salary' => request('salary'),
-    ]);
-    
-    // Validate
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required']
-    ]);
+        Gate::authorize('edit-job', $job);
 
+        request()->validate([
+            'title' => ['required', 'min:3'],
+            'salary' => ['required']
+        ]);
 
+        $job->update([
+            'title' => request('title'),
+            'salary' => request('salary'),
+        ]);
 
-    return redirect('/jobs/' . $job->id);
-    }
-    public function destroy(Job $job) 
-    {
-    // authorize (On hold...)
-
-    $job->delete();
-
-    return redirect('/jobs');
+        return redirect('/jobs/' . $job->id);
     }
 
+    public function destroy(Job $job)
+    {
+        Gate::authorize('edit-job', $job);
+
+        $job->delete();
+
+        return redirect('/jobs');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,28 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionController;
-
+use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
 
-Route::resource('jobs', JobController::class)->middleware('auth');
+ Route::get('/jobs', [JobController::class, 'index']);
+ Route::get('/jobs/create', [JobController::class, 'create']);
+ Route::post('/jobs', [JobController::class, 'store'])
+ ->middleware('auth');
 
-// Auth Routes
+ Route::get('/jobs/{job}', [JobController::class, 'show']);
 
+ Route::get('/jobs/{job}/edit', [JobController::class, 'edit'])
+     ->middleware('auth')
+     ->can('edit', 'job');
+
+ Route::patch('/jobs/{job}', [JobController::class, 'update']);
+ Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
+
+// Auth
 Route::get('/register', [RegisteredUserController::class, 'create']);
 Route::post('/register', [RegisteredUserController::class, 'store']);
 
