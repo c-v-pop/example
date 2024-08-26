5edc8cf131ec249f4f67b6dc83017878bf89bc7a
ciprian <ciprianvlad888@gmail.com>
2024-08-26 20:04:38 +0100

    installed npm and configured vite

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>My Website</title>
-    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body class="h-full">
